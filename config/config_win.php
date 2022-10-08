4c05c6145953a807f04ef314e92682548eb0f68a
wojtekxtx <wojtekx.tx@gmail.com>
2022-10-08 14:11:18 +0200

    Create config_win.php
    First, ALPHA, version
    
    Co-Authored-By: Thibault "bui" Koechlin <990714+buixor@users.noreply.github.com>

---
+++ config/config_win.php
@@ -0,0 +1,12 @@
+<?php
+class config_win {
+	
+	static protected $_root= null;
+
+	static public function root() {
+	 if (is_null(self::$_root)) self::$_root= dirname(__FILE__);
+	 return self::$_root;
+	}
+
+	
+}
