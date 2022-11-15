9bcf852504545f652cfa10cb4ac97588dff67592
wojtekxtx <wojtekx.tx@gmail.com>
2022-11-15 22:16:42 +0100

    added error msgs ( corresponding to schemas )

--- pkg/database/errors.go
+++ pkg/database/errors.go
@@ -3,20 +3,27 @@ package database
 import "errors"
 
 var (
-	UserExists        = errors.New("user already exist")
-	UserNotExists     = errors.New("user doesn't exist")
-	HashError         = errors.New("unable to hash")
-	InsertFail        = errors.New("unable to insert row")
-	QueryFail         = errors.New("unable to query")
-	UpdateFail        = errors.New("unable to update")
-	DeleteFail        = errors.New("unable to delete")
-	ItemNotFound      = errors.New("object not found")
-	ParseTimeFail     = errors.New("unable to parse time")
-	ParseDurationFail = errors.New("unable to parse duration")
-	MarshalFail       = errors.New("unable to marshal")
-	UnmarshalFail     = errors.New("unable to unmarshal")
-	BulkError         = errors.New("unable to insert bulk")
-	ParseType         = errors.New("unable to parse type")
-	InvalidIPOrRange  = errors.New("invalid ip address / range")
-	InvalidFilter     = errors.New("invalid filter")
+
+	// General errors
+	UserExists        			= errors.New("user already exist")
+	UserNotExists     			= errors.New("user doesn't exist")
+	HashError         			= errors.New("unable to hash")
+	InsertFail        			= errors.New("unable to insert row")
+	QueryFail         			= errors.New("unable to query")
+	UpdateFail        			= errors.New("unable to update")
+	DeleteFail        			= errors.New("unable to delete")
+	ItemNotFound      			= errors.New("object not found")
+	ParseTimeFail     			= errors.New("unable to parse time")
+	ParseDurationFail 			= errors.New("unable to parse duration")
+	MarshalFail       			= errors.New("unable to marshal")
+	UnmarshalFail     			= errors.New("unable to unmarshal")
+	BulkError         			= errors.New("unable to insert bulk")
+	ParseType         			= errors.New("unable to parse type")
+
+	// Filtering errors
+	InvalidFilter     			= errors.New("invalid filter")
+	InvalidDecision	  			= errors.New("Decision not valid due to errors (either codelike or semantic)")
+	Ip4NotInRange      			= errors.New("Given IP is out of range for IPv4")
+	RangeNotValidOrNotSpecified = errors.New("Provided range is not valid CIDR range")
+	FilterNotApplied			= errors.New("Filter has not been applied")
 )
